4c9a037f7e255e41bc96641931af55c921302241
panghongye <cc_@live.com>
2018-12-28 15:54:45 +0800

    0

--- web-server/lib/dao/mysql/dao-pool.js
+++ web-server/lib/dao/mysql/dao-pool.js
@@ -1,32 +1,36 @@
-var _poolModule = require('generic-pool');
-var mysqlConfig = require('../../../../shared/config/mysql');
-var mysql = require('mysql');
+var _poolModule = require("generic-pool")
+var mysqlConfig = require("../../../../shared/config/mysql.json")
 
-var env = process.env.NODE_ENV || 'development';
-if(mysqlConfig[env]) {
-  mysqlConfig = mysqlConfig[env];
+var mysql = require("mysql")
+
+var env = process.env.NODE_ENV || "development"
+if (mysqlConfig[env]) {
+  mysqlConfig = mysqlConfig[env]
 }
 
 /*
  * Create mysql connection pool.
  */
-var createMysqlPool = function(){
+var createMysqlPool = function() {
   return _poolModule.createPool({
-    name     : 'mysql',
-    create   : function(callback) {
+    name: "mysql",
+    create: function() {
       var client = mysql.createConnection({
         host: mysqlConfig.host,
         user: mysqlConfig.user,
         password: mysqlConfig.password,
-        database: mysqlConfig.database
-      });
-      return client;
+        database: mysqlConfig.database,
+      })
+      // callback(null, client);
+      return client
     },
-    destroy  : function(client) { client.end(); },
-    max      : 10,
-    idleTimeoutMillis : 30000,
-    log : false
-  });
-};
+    destroy: function(client) {
+      client.disconnect()
+    },
+    max: 10,
+    idleTimeoutMillis: 30000,
+    log: false,
+  })
+}
 
-exports.createMysqlPool = createMysqlPool;
+exports.createMysqlPool = createMysqlPool
--- web-server/lib/dao/mysql/mysql.js
+++ web-server/lib/dao/mysql/mysql.js
@@ -1,69 +1,65 @@
 // mysql CRUD
-var sqlclient = module.exports;
+var sqlclient = module.exports
 
-var _pool = null;
+var _pool = null
 
-var NND = {};
+var NND = {}
 
 /*
  * Innit sql connection pool
  * @param {Object} app The app for the server.
  */
-NND.init = function(){
-	if(!_pool)
-		_pool = require('./dao-pool').createMysqlPool();
-};
+NND.init = function() {
+  if (!_pool) _pool = require("./dao-pool").createMysqlPool()
+}
 
 /**
  * Excute sql statement
  * @param {String} sql Statement The sql need to excute.
  * @param {Object} args The args for the sql.
  * @param {fuction} callback Callback function.
- * 
+ *
  */
-NND.query = function(sql, args, callback){
-	_pool.acquire().then((client) => {
-        client.query(sql, args, function(err, res) {
-            _pool.release(client);
-            callback.apply(null, [err, res]);
-        });
-	}).catch((err) => {
-		console.log(err.stack);
-	});
-};
+NND.query = function(sql, args, callback) {
+  _pool
+    .acquire()
+    .then(client => {
+      client.query(sql, args, function(err, res) {
+        _pool.release(client)
+        callback.apply(null, [err, res])
+      })
+    })
+    .catch(err => {
+      console.log(err.stack)
+    })
+}
 
 /**
  * Close connection pool.
  */
-NND.shutdown = function(){
-	_pool.destroyAllNow();
-};
+NND.shutdown = function() {
+  _pool.destroyAllNow()
+}
 
 /**
  * init database
  */
 sqlclient.init = function() {
-	if (!!_pool){
-		return sqlclient;
-	} else {
-		NND.init();
-		sqlclient.insert = NND.query;
-		sqlclient.update = NND.query;
-		//sqlclient.delete = NND.query;
-		sqlclient.query = NND.query;
-    return sqlclient;
-	}
-};
+  if (!!_pool) {
+    return sqlclient
+  } else {
+    NND.init()
+    sqlclient.insert = NND.query
+    sqlclient.update = NND.query
+    //sqlclient.delete = NND.query;
+    sqlclient.query = NND.query
+    return sqlclient
+  }
+}
 
 /**
  * shutdown database
  */
 sqlclient.shutdown = function() {
-	NND.shutdown();
-};
-
-
-
-
-
-
+  NND.shutdown()
+}
