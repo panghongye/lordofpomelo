bbc47e89dce3d4bd0514bc047915b799bc6a9d39
panghongye <cc_@live.com>
2018-12-28 22:25:28 +0800

    9

--- game-server/app/ai/ai.js
+++ game-server/app/ai/ai.js
@@ -1,22 +1,22 @@
-var AiManager = require('./service/aiManager');
-var BrainService = require('./service/brainService');
-var fs = require('fs');
-var path = require('path');
+var AiManager = require("./service/aiManager")
+var BrainService = require("./service/brainService")
+var fs = require("fs")
+var path = require("path")
 
-var exp = module.exports;
+var exp = module.exports
 
 exp.createManager = function(opts) {
-	var brainService = new BrainService();
-	fs.readdirSync(__dirname + '/brain').forEach(function(filename){
-		if (!/\.js$/.test(filename)) {
-			return;
-		}
-		var name = path.basename(filename, '.js');
-		var brain = require('./brain/' + name);
-		brainService.registerBrain(brain.name||name, brain);
-	});
+  var brainService = new BrainService()
+  fs.readdirSync(__dirname + "/brain").forEach(function(filename) {
+    if (!/\.js$/.test(filename)) {
+      return
+    }
+    var name = path.basename(filename, ".js")
+    var brain = require("./brain/" + name)
+    brainService.registerBrain(brain.name || name, brain)
+  })
 
-	opts = opts || {};
-	opts.brainService = brainService;
-	return new AiManager(opts);
-};
+  opts = opts || {}
+  opts.brainService = brainService
+  return new AiManager(opts)
+}
--- game-server/app/ai/service/aiManager.js
+++ game-server/app/ai/service/aiManager.js
@@ -1,25 +1,25 @@
-var Blackboard = require('../meta/blackboard');
+var Blackboard = require("../meta/blackboard")
 
-var exp = module.exports;
+var exp = module.exports
 
 var Manager = function(opts) {
-	this.brainService = opts.brainService;
-	this.area = opts.area;
-	this.players = {};
-	this.mobs = {};
-};
+  this.brainService = opts.brainService
+  this.area = opts.area
+  this.players = {}
+  this.mobs = {}
+}
 
-module.exports = Manager;
+module.exports = Manager
 
-var pro = Manager.prototype;
+var pro = Manager.prototype
 
 pro.start = function() {
-	this.started = true;
-};
+  this.started = true
+}
 
 pro.stop = function() {
-	this.closed = true;
-};
+  this.closed = true
+}
 
 /**
  * Add a character into ai manager.
@@ -27,76 +27,81 @@ pro.stop = function() {
  * Start the tick if it has not started yet.
  */
 pro.addCharacters = function(cs) {
-	if(!this.started || this.closed) {
-		return;
-	}
+  if (!this.started || this.closed) {
+    return
+  }
 
-	if(!cs || !cs.length) {
-		return;
-	}
+  if (!cs || !cs.length) {
+    return
+  }
 
-	//create brain for the character.
-	//TODO: add a brain pool?
-	var c;
-	for(var i=0, l=cs.length; i<l; i++) {
-		c = cs[i];
-		var brain;
-		if(c.type === 'player') {
-			if(this.players[c.entityId]) {
-				continue;
-			}
+  //create brain for the character.
+  //TODO: add a brain pool?
+  var c
+  for (var i = 0, l = cs.length; i < l; i++) {
+    c = cs[i]
+    var brain
+    if (c.type === "player") {
+      if (this.players[c.entityId]) {
+        continue
+      }
 
-			brain = this.brainService.getBrain('player', Blackboard.create({
-				manager: this,
-				area: this.area,
-				curCharacter: c
-			}));
-			this.players[c.entityId] = brain;
-		} else {
-			if(this.mobs[c.entityId]) {
-				continue;
-			}
+      brain = this.brainService.getBrain(
+        "player",
+        Blackboard.create({
+          manager: this,
+          area: this.area,
+          curCharacter: c,
+        })
+      )
+      this.players[c.entityId] = brain
+    } else {
+      if (this.mobs[c.entityId]) {
+        continue
+      }
 
-			brain = this.brainService.getBrain(c.characterName, Blackboard.create({
-				manager: this,
-				area: this.area,
-				curCharacter: c
-			}));
-			this.mobs[c.entityId] = brain;
-		}
-	}
-};
+      brain = this.brainService.getBrain(
+        c.characterName,
+        Blackboard.create({
+          manager: this,
+          area: this.area,
+          curCharacter: c,
+        })
+      )
+      this.mobs[c.entityId] = brain
+    }
+  }
+}
 
 /**
  * remove a character by id from ai manager
  */
 pro.removeCharacter = function(id) {
-	if(!this.started || this.closed) {
-		return;
-	}
+  if (!this.started || this.closed) {
+    return
+  }
 
-	delete this.players[id];
-	delete this.mobs[id];
-};
+  delete this.players[id]
+  delete this.mobs[id]
+}
 
 /**
  * Update all the managed characters.
  * Stop the tick if there is no ai mobs.
  */
 pro.update = function() {
-	if(!this.started || this.closed) {
-		return;
-	}
-	var id;
-	for(id in this.players) {
-		if(typeof this.players[id].update === 'function') {
-			this.players[id].update();
-		}
-	}
-	for(id in this.mobs) {
-		if(typeof this.mobs[id].update === 'function') {
-			this.mobs[id].update();
-		}
-	}
-};
-
+  if (!this.started || this.closed) {
+    return
+  }
+  var id
+  for (id in this.players) {
+    if (typeof this.players[id].update === "function") {
+      this.players[id].update()
+    }
+  }
+  for (id in this.mobs) {
+    if (typeof this.mobs[id].update === "function") {
+      this.mobs[id].update()
+    }
+  }
+}
